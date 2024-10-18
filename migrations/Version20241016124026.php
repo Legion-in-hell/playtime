3f70aa86b2a8d13363f1e3a7b81356f53bd508ff
Corentin Rouff <Rouff.corentin@hotmail.fr>
2024-10-18 10:20:15 +0200

    Refactor database schema to remove unnecessary foreign key constraints and indices

--- migrations/Version20241016124026.php
+++ migrations/Version20241016124026.php
@@ -20,7 +20,6 @@ final class Version20241016124026 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE guest_reservation ADD CONSTRAINT FK_C5FDF2D78A2D8B41 FOREIGN KEY (terrain_id) REFERENCES terrain (id)');
         $this->addSql('CREATE INDEX IDX_C5FDF2D78A2D8B41 ON guest_reservation (terrain_id)');
         $this->addSql('ALTER TABLE reservation ADD terrain_id INT NOT NULL');
         $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C849558A2D8B41 FOREIGN KEY (terrain_id) REFERENCES terrain (id)');
@@ -33,7 +32,6 @@ final class Version20241016124026 extends AbstractMigration
         $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C849558A2D8B41');
         $this->addSql('DROP INDEX IDX_42C849558A2D8B41 ON reservation');
         $this->addSql('ALTER TABLE reservation DROP terrain_id');
-        $this->addSql('ALTER TABLE guest_reservation DROP FOREIGN KEY FK_C5FDF2D78A2D8B41');
         $this->addSql('DROP INDEX IDX_C5FDF2D78A2D8B41 ON guest_reservation');
     }
 }
